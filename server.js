b89775fa5e21274415586a4926034f2a799484eb
Joris Compernol <joris.compernol@engie.com>
2019-11-20 23:24:14 +0100

    form confirmation

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const express = require ('express');
-const app = express ();
-var cors = require ('cors');
-const path = require ('path');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-var helper = require ('sendgrid').mail;
-var sg = require ('sendgrid') (process.env.SENDGRID_API_KEY);
+var helper = require('sendgrid').mail;
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
-// const CONTACT_ADDRESS = 'sangoo@alveare-change.com';
-const CONTACT_ADDRESS = 'joriscompernol@solenoid.be';
+const CONTACT_ADDRESS = 'sangoo@alveare-change.com';
+// const CONTACT_ADDRESS = 'joriscompernol@solenoid.be';
 
-app.use (cors ());
-app.use (express.static (path.join (__dirname, 'dist')));
-app.use (require ('body-parser').json ());
-app.post ('/contact', (req, res) => {
-  var from_email = new helper.Email (req.body.from);
-  var to_email = new helper.Email (CONTACT_ADDRESS);
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(require('body-parser').json());
+app.post('/contact', (req, res) => {
+  var from_email = new helper.Email(req.body.from);
+  var to_email = new helper.Email(CONTACT_ADDRESS);
   var subject = 'New message from website';
-  var content = new helper.Content ('text/html', parseBodyToHtml (req.body));
-  var mail = new helper.Mail (from_email, subject, to_email, content);
+  var content = new helper.Content('text/html', parseBodyToHtml(req.body));
+  var mail = new helper.Mail(from_email, subject, to_email, content);
 
-  var request = sg.emptyRequest ({
+  var request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
-    body: mail.toJSON (),
+    body: mail.toJSON(),
   });
 
-  sg.API (request, function (apiError, apiResponse) {
+  sg.API(request, function(apiError, apiResponse) {
     if (apiError) {
-      console.log ('apiError', apiError);
-      res.json (apiError);
+      console.log('apiError', apiError);
+      res.json(apiError);
     } else {
-      res.status (apiResponse.statusCode);
-      res.json (apiResponse);
+      res.status(apiResponse.statusCode);
+      res.json(apiResponse);
     }
   });
 });
-app.listen (PORT, () => console.log (`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-function parseBodyToHtml (obj) {
+function parseBodyToHtml(obj) {
   let str = '';
   for (i in obj) {
     str += i + ': ' + obj[i] + '</br>';
